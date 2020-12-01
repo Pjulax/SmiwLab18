956d86960695c5b069b9fc585125c10699e82434
Paweł Potuczko <pawelpo99@gmail.com>
2020-12-01 16:57:28 +0100

    all working properly

--- AudioSmiwLab18/AudioSmiwLab18.cpp
+++ AudioSmiwLab18/AudioSmiwLab18.cpp
@@ -3,6 +3,7 @@
 
 void echoZad1Transform(std::ifstream& input, std::ofstream& output);
 void echoZad2Transform(std::ifstream& input, std::ofstream& output);
+void echoZad3Transform(std::ifstream& input, std::ofstream& output);
 
 int main()
 {
@@ -11,23 +12,24 @@ int main()
 
     echoZad1Transform(rawInput, rawOutput);
     echoZad2Transform(rawInput, rawOutput);
+    echoZad3Transform(rawInput, rawOutput);
 
     return 0;
 }
 
 void echoZad1Transform(std::ifstream& input, std::ofstream& output) {
-    input.open("123.raw", std::ios::binary);
-    output.open("123Zad1.raw", std::ios::binary);
+    input.open("12.raw", std::ios::binary);
+    output.open("12Zad1.raw", std::ios::binary);
 
     char sampleInput[2];
-    uint16_t sample;
-    uint16_t* actualSecond = new uint16_t[44100];   // actual second of sound
-    uint16_t* lastSecond = new uint16_t[44100];     // last second of sound
+    short int sample;
+    short int* actualSecond = new short int[44100]{ 0 };   // actual second of sound
+    short int* lastSecond = new short int[44100]{ 0 };     // last second of sound
 
     int i = 0;
     if (input.is_open()) {
         // first second without echo loop
-        while (i < 44100 && input.read(sampleInput, sizeof(uint16_t))) {
+        while (i < 44100 && input.read(sampleInput, sizeof(short int))) {
             sample = sampleInput[0] << 8;
             sample += sampleInput[1];
             actualSecond[i++] = sample;
@@ -35,11 +37,11 @@ void echoZad1Transform(std::ifstream& input, std::ofstream& output) {
             sampleInput[1] = sample;
             sampleInput[0] = sample >> 8;
 
-            output.write(sampleInput, sizeof(uint16_t));
+            output.write(sampleInput, sizeof(short int));
         }
         // all with echo loop
         i = 0;
-        while (input.read(sampleInput, sizeof(uint16_t))) {
+        while (input.read(sampleInput, sizeof(short int))) {
             if (i == 44100)
                 i = 0;
             sample = sampleInput[0] << 8;
@@ -55,7 +57,7 @@ void echoZad1Transform(std::ifstream& input, std::ofstream& output) {
             sampleInput[1] = sample;
             sampleInput[0] = sample >> 8;
 
-            output.write(sampleInput, sizeof(uint16_t));
+            output.write(sampleInput, sizeof(short int));
             i++;
         }
         // last echo loop
@@ -65,7 +67,7 @@ void echoZad1Transform(std::ifstream& input, std::ofstream& output) {
             sampleInput[1] = sample;
             sampleInput[0] = sample >> 8;
 
-            output.write(sampleInput, sizeof(uint16_t));
+            output.write(sampleInput, sizeof(short int));
             i++;
         }
         input.close();
@@ -76,24 +78,24 @@ void echoZad1Transform(std::ifstream& input, std::ofstream& output) {
 }
 
 void echoZad2Transform(std::ifstream& input, std::ofstream& output) {
-    input.open("123.raw", std::ios::binary);
-    output.open("123Zad2.raw", std::ios::binary);
+    input.open("12.raw", std::ios::binary);
+    output.open("12Zad2.raw", std::ios::binary);
 
     char sampleInput[2];
-    uint16_t sample;
-    uint16_t echoSample;
-    uint16_t* actualSeconds = new uint16_t[44100];   // actual second of sound
-    uint16_t* firstEchoSeconds = new uint16_t[44100];
-    uint16_t* secondEchoSeconds = new uint16_t[44100];
-    uint16_t* thirdEchoSeconds = new uint16_t[44100];
-    uint16_t* fourthEchoSeconds = new uint16_t[44100];
+    short int sample;
+    short int echoSample;
+    short int* actualSeconds = new short int[44100]{ 0 };   // actual second of sound
+    short int* firstEchoSeconds = new short int[44100]{ 0 };
+    short int* secondEchoSeconds = new short int[44100]{ 0 };
+    short int* thirdEchoSeconds = new short int[44100]{ 0 };
+    short int* fourthEchoSeconds = new short int[44100]{ 0 };
 
 
     int i = 0;
     int echoCount = 0;
     if (input.is_open()) {
         // first seconds without echo loop
-        while (i < 44100 && input.read(sampleInput, sizeof(uint16_t))) {
+        while (i < 44100 && input.read(sampleInput, sizeof(short int))) {
             sample = sampleInput[0] << 8;
             sample += sampleInput[1];
             actualSeconds[i++] = sample;
@@ -101,11 +103,11 @@ void echoZad2Transform(std::ifstream& input, std::ofstream& output) {
             sampleInput[1] = sample;
             sampleInput[0] = sample >> 8;
 
-            output.write(sampleInput, sizeof(uint16_t));
+            output.write(sampleInput, sizeof(short int));
         }
         // all with echo loop
         i = 0;
-        while (input.read(sampleInput, sizeof(uint16_t))) {
+        while (input.read(sampleInput, sizeof(short int))) {
             if (i == 44100) {
                 i = 0;
                 if (echoCount <= 3 && echoCount >= 0)
@@ -117,18 +119,18 @@ void echoZad2Transform(std::ifstream& input, std::ofstream& output) {
             echoSample = 0;
             // == transform ==
             if (echoCount >= 4)
-                echoSample = uint16_t(fourthEchoSeconds[i] * 0.1f);
+                echoSample = fourthEchoSeconds[i];
             if (echoCount >= 3) {
                 fourthEchoSeconds[i] = thirdEchoSeconds[i];
-                echoSample = echoSample + uint16_t(thirdEchoSeconds[i] * 0.2f);
+                echoSample = echoSample + thirdEchoSeconds[i];
             }
             if (echoCount >= 2) {
                 thirdEchoSeconds[i] = secondEchoSeconds[i];
-                echoSample = echoSample + uint16_t(secondEchoSeconds[i] * 0.3f);
+                echoSample = echoSample + secondEchoSeconds[i];
             }
             if (echoCount >= 1) {
                 secondEchoSeconds[i] = firstEchoSeconds[i];
-                echoSample = echoSample + uint16_t(firstEchoSeconds[i] * 0.4f);
+                echoSample = echoSample + firstEchoSeconds[i];
             }
             firstEchoSeconds[i] = actualSeconds[i];
             sample += echoSample;
@@ -139,7 +141,7 @@ void echoZad2Transform(std::ifstream& input, std::ofstream& output) {
             sampleInput[1] = sample;
             sampleInput[0] = sample >> 8;
 
-            output.write(sampleInput, sizeof(uint16_t));
+            output.write(sampleInput, sizeof(short int));
             i++;
         }
 
@@ -153,17 +155,17 @@ void echoZad2Transform(std::ifstream& input, std::ofstream& output) {
             }
             sample = 0;
             if (echoCount >= 4)
-                sample = uint16_t(fourthEchoSeconds[i] * 0.1f);
+                sample = fourthEchoSeconds[i];
             if (echoCount >= 3)
-                sample = sample +  uint16_t(thirdEchoSeconds[i] * 0.2f);
+                sample = sample + thirdEchoSeconds[i];
             if (echoCount >= 2)
-                sample = sample + uint16_t(secondEchoSeconds[i] * 0.3f);
+                sample = sample + secondEchoSeconds[i];
             if (echoCount >= 1) 
-                sample = sample + uint16_t(firstEchoSeconds[i] * 0.4f);
+                sample = sample + firstEchoSeconds[i];
             sampleInput[1] = sample;
             sampleInput[0] = sample >> 8;
 
-            output.write(sampleInput, sizeof(uint16_t));
+            output.write(sampleInput, sizeof(short int));
             i++;
         }
 
@@ -176,6 +178,58 @@ void echoZad2Transform(std::ifstream& input, std::ofstream& output) {
         delete[] fourthEchoSeconds;
     }
 }
+
+void echoZad3Transform(std::ifstream& input, std::ofstream& output) {
+    input.open("12.raw", std::ios::binary);
+    output.open("12Zad3.raw", std::ios::binary);
+
+    char sampleInput[2];
+    short int* lastHighByte = new short int[44100]{ 0 };     // last second of sound
+    short int* lastLowByte = new short int[44100]{ 0 };     // last second of sound
+
+    int i = 0;
+    if (input.is_open()) {
+        // first second without echo loop
+        while (i < 44100 && input.read(sampleInput, sizeof(short int))) {
+            lastLowByte[i] = ((char)sampleInput[0] << 8) * 0.6f;
+            lastHighByte[i] += sampleInput[1] * 0.6f;
+            i++;
+            output.write(sampleInput, sizeof(short int));
+        }
+        // all with echo loop
+        i = 0;
+        while (input.read(sampleInput, sizeof(short int))) {
+            if (i == 44100)
+                i = 0;
+            lastLowByte[i] = ((char)sampleInput[0] << 8) + lastLowByte[i] * 0.6f;
+            lastHighByte[i] = sampleInput[1] + lastHighByte[i] * 0.6f;
+
+            sampleInput[1] = lastHighByte[i];
+            sampleInput[0] = lastLowByte[i];
+
+            output.write(sampleInput, sizeof(short int));
+            i++;
+        }
+        // last echo loop
+        i = 0;
+        while (i < 44100) {
+            if (i == 44100)
+                i = 0;
+            if (lastLowByte[i] <= 3 && lastHighByte[i] <= 3)
+                break;
+            lastLowByte[i] = lastLowByte[i] * 0.6f;
+            lastHighByte[i] = lastHighByte[i] * 0.6f;
+
+            output.write(sampleInput, sizeof(short int));
+            i++;
+        }
+        input.close();
+        output.close();
+        delete[] lastLowByte;
+        delete[] lastHighByte;
+    }
+}
+
 
 /*
 int i = 0;
